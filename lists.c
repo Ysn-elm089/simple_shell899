7ff9f63020e2ca6fd24a16e2d3cb6c2d82ef2df9
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-08-22 05:27:49 -0700

    second half

--- lists.c
+++ lists.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str, int nu)
 	if (!new_head)
 		return (NULL);
 	_memset((void *)new_head, 0, sizeof(list_t));
-	new_head->num = num;
+	new_head->num = nu;
 	if (str)
 	{
 		new_head->str = _strdup(str);
@@ -53,7 +53,7 @@ list_t *add_node_end(list_t **head, const char *str, int nu)
 	if (!new_node)
 		return (NULL);
 	_memset((void *)new_node, 0, sizeof(list_t));
-	new_node->num = num;
+	new_node->num = nu;
 	if (str)
 	{
 		new_node->str = _strdup(str);
